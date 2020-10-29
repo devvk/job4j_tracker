68ce649c0269a834be7cbf9a05748f805dd9acec
devvk <info@sekretov.net>
2020-10-29 12:16:57 +0100

    3. Реализация профессий в коде [#353076]

---
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Builder extends Engineer {
+    private boolean canStatics;
+
+    public boolean getCanStatics() {
+        return canStatics;
+    }
+}
+
---
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class Surgeon extends Doctor {
+    private boolean canOperate;
+
+    public boolean getCanOperate() {
+        return canOperate;
+    }
+}
