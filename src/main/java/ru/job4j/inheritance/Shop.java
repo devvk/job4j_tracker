fe96777ca315bdf30665b7108661cdde55bd6e1b
devvk <info@sekretov.net>
2020-10-30 13:31:22 +0100

    3.1. Массив с пустыми ячейками [#353081]

--- src/main/java/ru/job4j/inheritance/Shop.java
+++ src/main/java/ru/job4j/inheritance/Shop.java
@@ -8,9 +8,9 @@ public class Shop {
     }
 
     public static Product of(String name, int price) {
-        if ("Oil".equals(name)) {
-            return new LiquidationProduct(name, price);
-        }
+        //if ("Oil".equals(name)) {
+        //    return new LiquidationProduct(name, price);
+        //}
         return new Product(name, price);
     }
 }
