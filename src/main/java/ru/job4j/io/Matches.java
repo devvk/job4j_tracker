d92d2ff8c756f859a7563ee4f1b53ba15fe4c1df
devvk <info@sekretov.net>
2020-11-04 11:20:31 +0100

    1. Чтение из консоли. Класс Scanner. [#353006]

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        int count = 11;
+        while (count > 0) {
+            System.out.print("Введите число от 1 до 3 для игрока №1: ");
+            Scanner input1 = new Scanner(System.in);
+            int score1 = Integer.valueOf(input1.nextLine());
+            count -= score1;
+            System.out.println("Количество спичек на столе: " + count);
+            System.out.print("Введите число от 1 до 3 для игрока №2: ");
+            Scanner input2 = new Scanner(System.in);
+            int score2 = Integer.valueOf(input2.nextLine());
+            count -= score2;
+            System.out.println("Количество спичек на столе: " + count);
+        }
+    }
+}
