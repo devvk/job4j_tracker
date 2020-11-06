a59efdf1eeadcd0e480ba95d4f6240011dfc072e
devvk <info@sekretov.net>
2020-11-06 10:33:28 +0100

    5.1. Тестирование. Подготовка данных. [#353015]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,36 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    /**
+     * Порядок действий.
+     * 1. Создаем объект tracker.
+     * 2. Создаем объект item.
+     * 3. Добавляем item в tracker. После этой операции у нас есть id.
+     * 4. Достаем item.id и создаем массив с ответами пользователя.
+     * 5. Вызываем тестируемый метод replaceItem. Это действие изменит состояние объекта tracker.
+     * 6. Ищем по item.id замененный item в объекте tracker.
+     * 7. Сравниваем имя найденной заявки с ожидаемой.
+     */
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
+    }
 }
