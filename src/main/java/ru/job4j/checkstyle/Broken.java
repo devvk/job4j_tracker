f89794fc41e7e8ad292d59905457479d9db64654
devvk <info@sekretov.net>
2023-02-17 18:43:43 +0100

    Правила Checkstyle [#504981 #392356]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,42 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private final int size = 10;
+
+    private final String name;
+
+    private final String surname;
+
+    public static final String NEW_VALUE = "test";
+
+    public Broken(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    void echo() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("size: " + size);
+        System.out.println(NEW_VALUE);
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+        System.out.println(size + a + b + c + d + e + f + g);
+    }
+
+    public static void main(String[] args) {
+        Broken broken = new Broken("Alex", "Johnson");
+        broken.echo();
+        for (int i = 1; i < 5; i++) {
+            broken.method(i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7);
+        }
+        broken.media(broken);
+    }
+}
