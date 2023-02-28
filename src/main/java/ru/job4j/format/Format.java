432c41b878ed3d40beaa38c4fd64dc1b572454e9
devvk <info@sekretov.net>
2023-02-28 11:42:06 +0100

    9. Форматирование данных. Методы format() и printf() [#504975 #396018]

---
+++ src/main/java/ru/job4j/format/Format.java
@@ -0,0 +1,27 @@
+package ru.job4j.format;
+
+import java.util.Calendar;
+
+/**
+ * <a href="https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/util/Formatter.html">
+ * https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/util/Formatter.html
+ * </a>
+ */
+public class Format {
+
+    public static void getGreeting(String name) {
+        System.out.printf("Hello %s.%n", name);
+    }
+
+    public static void main(String[] args) {
+        String name = "String.format()";
+        String text = String.format("Тестирование метода: %s", name);
+        System.out.println(text);
+        getGreeting("student");
+        System.out.println(String.format("Jo%c4j", 'b'));
+        System.out.format("Строка1%nСтрока2%n");
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(String.format("Date: %tF, Time: %tT", calendar, calendar));
+        System.out.println(String.format("Число с плавающей точкой: %.2f", 0.123456789));
+    }
+}
