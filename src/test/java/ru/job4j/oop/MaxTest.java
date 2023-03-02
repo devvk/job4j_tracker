b9f6c811f9f8f2d2957bbea5b62bc8abefe8e925
devvk <info@sekretov.net>
2023-03-02 19:05:17 +0100

    MaxTest

--- src/test/java/ru/job4j/oop/MaxTest.java
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -1,62 +1,61 @@
 package ru.job4j.oop;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
     @Test
     public void whenMax2To1Then2() {
         int result = Max.max(2, 1);
-        assertThat(result, is(2));
+        assertThat(result).isEqualTo(2);
     }
 
     @Test
     public void whenMax1To2Then2() {
         int result = Max.max(1, 2);
-        assertThat(result, is(2));
+        assertThat(result).isEqualTo(2);
     }
 
     @Test
     public void whenMax2To2Then2() {
         int result = Max.max(2, 2);
-        assertThat(result, is(2));
+        assertThat(result).isEqualTo(2);
     }
 
     @Test
     public void whenMax3To1And2Then3() {
         int result = Max.max(2, 1, 3);
-        assertThat(result, is(3));
+        assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void whenMax6To1And3Then6() {
         int result = Max.max(6, 1, 3);
-        assertThat(result, is(6));
+        assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void whenMax6To6And6Then6() {
         int result = Max.max(6, 6, 6);
-        assertThat(result, is(6));
+        assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void whenMax13To1And2And3Then13() {
         int result = Max.max(1, 2, 3, 13);
-        assertThat(result, is(13));
+        assertThat(result).isEqualTo(13);
     }
 
     @Test
     public void whenMax5To4And1And3Then5() {
         int result = Max.max(5, 4, 1, 3);
-        assertThat(result, is(5));
+        assertThat(result).isEqualTo(5);
     }
 
     @Test
     public void whenMax5To5And5And5Then5() {
         int result = Max.max(5, 5, 5, 5);
-        assertThat(result, is(5));
+        assertThat(result).isEqualTo(5);
     }
 }
