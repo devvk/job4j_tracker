168dde15aa142da132bf9f9b8f54cea2aedc351c
devvk <info@sekretov.net>
2023-03-02 18:06:16 +0100

    3.0. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,53 +1,38 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
 
-    private final Output out;
-
-    public  StartUI(Output out) {
-        this.out = out;
-    }
-
-    public void init(Input input, Tracker tracker, UserAction[] actions) {
+    public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
-            this.showMenu(actions);
-            int select = input.askInt("Select: ");
-            if (select < 0 || select >= actions.length) {
-                out.println("Wrong input, you can select: 0 - " + (actions.length - 1));
-                continue;
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
             }
-            UserAction action = actions[select];
-            run = action.execute(input, tracker);
         }
     }
 
-    private void showMenu(UserAction[] actions) {
-        out.println("Menu.");
-        for (int i = 0; i < actions.length; i++) {
-            out.println(i + ". " + actions[i].name());
-        }
-    }
-
-    /**
-     * 1. Мы получаем от пользователя пункт меню.
-     * 2. Этот параметр мы используем в качестве индекса в массиве actions.
-     * 3. Далее мы получаем из массива один из объектов UserAction (CreateAction, ReplaceAction...);
-     * 4. У полученного объекта вызываем метод execute с передачей параметров input и tracker.
-     */
-    public static void main(String[] args) {
-        Output output = new ConsoleOutput();
-        Input input = new ValidateInput(output, new ConsoleInput());
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(output),
-                new FindAllAction(output),
-                new ReplaceAction(output),
-                new DeleteAction(output),
-                new FindByIdAction(output),
-                new FindByNameAction(output),
-                new ExitAction(output),
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
         };
-        new StartUI(output).init(input, tracker, actions);
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
