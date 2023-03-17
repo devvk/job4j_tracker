1e77a933f47dde5b6adf4bcffb41ef128b5fc1dc
devvk <info@sekretov.net>
2023-03-17 15:39:41 +0100

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -4,20 +4,7 @@ package ru.job4j.tracker;
  * Класс ValidateInput получает данные из системы ввода до тех пор,
  * пока не введут правильные символы.
  */
-public class ValidateInput implements Input {
-
-    private final Output out;
-    private final Input in;
-
-    public ValidateInput(Output out, Input in) {
-        this.out = out;
-        this.in = in;
-    }
-
-    @Override
-    public String askStr(String question) {
-        return in.askStr(question);
-    }
+public class ValidateInput extends ConsoleInput {
 
     @Override
     public int askInt(String question) {
@@ -25,10 +12,10 @@ public class ValidateInput implements Input {
         int value = -1;
         do {
             try {
-                value = in.askInt(question);
+                value = super.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                out.println("Please enter validate data again.");
+                System.out.println("Please enter validate data.");
             }
         } while (invalid);
         return value;
