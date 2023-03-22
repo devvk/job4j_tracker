9950d57155876d72caf17b50fd5041a4fad9075b
devvk <info@sekretov.net>
2023-03-22 12:36:36 +0100

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -12,71 +12,52 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!containUpperCaseLetter(password)) {
-            throw new IllegalArgumentException("Password should contain at least one "
-                    + "uppercase letter");
-        }
-        if (!containLowerCaseLetter(password)) {
-            throw new IllegalArgumentException("Password should contain at least one "
-                    + "lowercase letter");
-        }
-        if (!containDigit(password)) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-        if (!containSpecialSymbol(password)) {
-            throw new IllegalArgumentException("Password should contain at least one "
-                    + "special symbol");
-        }
         if (containSubstrings(password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: "
                     + "qwerty, 12345, password, admin, user");
         }
+        return containSymbols(password);
+    }
+
+    private static String containSymbols(String password) {
+        boolean hasUppercase = false;
+        boolean hasLowercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUppercase = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLowercase = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                hasSpecial = true;
+            }
+        }
+
+        if (!hasUppercase) {
+            throw new IllegalArgumentException("Password should contain at least one "
+                    + "uppercase letter");
+        }
+        if (!hasLowercase) {
+            throw new IllegalArgumentException("Password should contain at least one "
+                    + "lowercase letter");
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException("Password should contain at least one "
+                    + "special symbol");
+        }
         return password;
     }
 
-    private static boolean containSubstrings(String s) {
+    private static boolean containSubstrings(String password) {
         for (String str : NOT_ALLOWED_PASSWORDS) {
-            if (s.toLowerCase().contains(str.toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(containSubstrings("Ln2$mrQWerTY12"));
-    }
-
-    private static boolean containSpecialSymbol(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isLetterOrDigit(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containDigit(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containLowerCaseLetter(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLowerCase(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containUpperCaseLetter(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isUpperCase(s.charAt(i))) {
+            if (password.toLowerCase().contains(str.toLowerCase())) {
                 return true;
             }
         }
