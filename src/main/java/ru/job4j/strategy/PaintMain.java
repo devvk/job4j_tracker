65cda17f670c09eb2bf33b23eb52bd58aff4a76a
devvk <info@sekretov.net>
2023-03-29 15:57:13 +0200

    11.0. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/PaintMain.java
+++ src/main/java/ru/job4j/strategy/PaintMain.java
@@ -1,12 +1,12 @@
 package ru.job4j.strategy;
 
-public class Paint {
+public class PaintMain {
     public void draw(Shape shape) {
         System.out.print(shape.draw());
     }
 
     public static void main(String[] args) {
-        Paint context = new Paint();
+        PaintMain context = new PaintMain();
         context.draw(new Triangle());
         context.draw(new Square());
     }
