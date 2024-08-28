84d314f582986208e7cf8395a2278ebfbef4a62c
devvk <info@sekretov.net>
2024-08-28 12:42:56 +0200

    1. Уникальные задачи [#212653]

---
+++ src/main/java/ru/job4j/collection/Task.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+public class Task {
+    private final String number;
+    private final String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    // Включите этот метод, если понадобится описание задачи
+    public String getDescription() {
+        return description;
+    }
+}
