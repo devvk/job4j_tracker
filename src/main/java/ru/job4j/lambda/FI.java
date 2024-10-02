fff66ea7ce133de1ec91e032d0b215501acb645b
devvk <info@sekretov.net>
2024-10-02 11:01:26 +0200

    2.0. Лямбда  [#58165]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        String[] strings = {"Donald Duck", "G", "A", "Ba", "Canada", "Faa"};
+
+        Comparator<String> comparatorText = (left, right) -> left.compareTo(right);
+        Arrays.sort(strings, comparatorText);
+        System.out.println(Arrays.toString(strings));
+
+        Comparator<String> comparatorDescSize = (left, right) -> right.length() - left.length();
+        Arrays.sort(strings, comparatorDescSize);
+        System.out.println(Arrays.toString(strings));
+    }
+}
