a17932c3a4ae7f4c828949a3a9fe1e4f5c2accea
devvk <info@sekretov.net>
2024-10-11 15:25:34 +0200

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,8 +1,17 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Profiles {
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::address)
+                .sorted(Comparator.comparing(Address::city))
+                .distinct()
+                .toList();
+    }
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -2,7 +2,6 @@ package ru.job4j.stream;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,11 +11,32 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class ProfilesTest {
 
     @Test
+    public void whenTestMethodCollectSortWithoutDuplicate() {
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        Address fourth = new Address("City1", "Street1", 1, 1);
+        Address fifth = new Address("City2", "Street2", 2, 2);
+        Address seventh = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = List.of(
+                new Profile(second),
+                new Profile(fourth),
+                new Profile(fifth),
+                new Profile(first),
+                new Profile(seventh),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collectSortWithoutDuplicate(profiles);
+        List<Address> expected = List.of(first, second, third);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void whenTestMethodCollect() {
         Address first = new Address("City1", "Street1", 1, 1);
         Address second = new Address("City2", "Street2", 2, 2);
         Address third = new Address("City3", "Street3", 3, 3);
-        List<Profile> profiles = Arrays.asList(
+        List<Profile> profiles = List.of(
                 new Profile(first),
                 new Profile(second),
                 new Profile(third)
